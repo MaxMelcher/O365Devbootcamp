ac00cdac88d4ec846d341f3d40f8c92d39a87ec4
Max Melcher <mmelcher@alegri.eu>
2017-10-19 23:52:44 +0200

    announcing new ones

--- O365DevBootcamp.Speakers/O365DevBootcamp.Speakers/Controllers/HomeController.cs
+++ O365DevBootcamp.Speakers/O365DevBootcamp.Speakers/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols;
 using O365DevBootcamp.Speakers.Data;
 using O365DevBootcamp.Speakers.Models;
 
@@ -48,6 +51,12 @@ namespace O365DevBootcamp.Speakers.Controllers
             return View();
         }
 
+        public async Task<IActionResult> List()
+        {
+            var list = await _db.QueryManyAsync<Attendee>("select * from Attendee");
+            return View(list.ToList());
+        }
+
         [HttpPost]
         public async Task<IActionResult> Checkin(string email, string fullname, string twitter, string blog, string picture)
         {
@@ -70,10 +79,31 @@ namespace O365DevBootcamp.Speakers.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Add(string email)
+        {
+            var attendee = await _db.QueryAsync<Attendee>("select top 1 * from Attendee where email = @email", new { email });
+
+
+            var client = new HttpClient();
+            var url = "https://outlook.office.com/webhook/4227e51c-d798-4c41-8e2c-9139d6afc5f2@5697ac0d-aec6-4e7d-9437-986d6cac2590/IncomingWebhook/821d15eb43414232a1e958e61582be7f/7924b20e-426e-4276-a710-d9ed98b152d5";
+            var message = $"{{\"text\": \"{attendee.Fullname} checked in.\",\"sections\": [{{\"activityTitle\": \"Check-in\",\"activityText\": \"{attendee.Email}\",\"activityImage\": \"{attendee.Picture}\"}}]}}";
+            var body = new StringContent(message, System.Text.Encoding.UTF8, "application/json");
+            await client.PostAsync(url, body);
+
+            
+            attendee.Added = true;
+            await _db.UpdateAsync(attendee);
+
+            return RedirectToAction("List");
+        }
     }
     [Table("Attendee")]
     public class Attendee
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Fullname { get; set; }
         public string Twitter { get; set; }
