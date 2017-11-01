4beb3dc307b23fb11b31b4120297562b8fa21dea
Max Melcher <mmelcher@alegri.eu>
2017-11-01 13:16:51 +0100

    Basic Bot

--- O365DevBootcamp/Dialogs/RootDialog.cs
+++ O365DevBootcamp/Dialogs/RootDialog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
@@ -21,19 +22,31 @@ namespace O365DevBootcamp.Dialogs
         {
             var activity = await result as Activity;
 
-            string replyText = activity.Text.Substring(activity.Text.LastIndexOf("</at>")+5).Trim();
-
-
-            if (replyText == "Yes")
+            if (activity.Text.Contains("Yes!"))
             {
+                Activity isTypingReply = activity.CreateReply("Thinking about it...");
+                isTypingReply.Type = ActivityTypes.Typing;
+                await context.PostAsync(isTypingReply);
+
+                Thread.Sleep(2000);
+
                 Activity reply = activity.CreateReply();
                 reply.Text = "Thats totally accurate!";
                 await context.PostAsync(reply);
+
+                //await context.PostAsync(isTypingReply);
+
+                //Thread.Sleep(2000);
+
+                //Activity reply2 = activity.CreateReply();
+                //reply2.Text = "... now I need to do important stuff!";
+                //reply2.Type = ActivityTypes.EndOfConversation;
+                //await context.PostAsync(reply2);
             }
             else
             {
                 Activity reply = activity.CreateReply();
-                reply.AddHeroCard("Bots are awesome!", new List<string> { "Yes", "Yes" }, new List<string> { "Awesome", "More awesome!" });
+                reply.AddHeroCard("Bots are awesome!", new List<string> { "Yes!", "Yes!" }, new List<string> { "Awesome", "More awesome!" });
                 await context.PostAsync(reply);
                 context.Wait(MessageReceivedAsync);
             }
